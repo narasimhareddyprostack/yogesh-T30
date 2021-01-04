d4f6bc90347c9a733d5882d8e5d4c020edcbb6bb
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-01-04 14:30:26 +0530

    context api

--- ReactB/src/App.js
+++ ReactB/src/App.js
@@ -1,3 +1,47 @@
+import React from "react";
+import "./App.css";
+//import ComponentA from "./components/props-drilling/ComponentA";
+import CompA from "./ContextAPI/CompA";
+import UserContext from "./ContextAPI/UserContext";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        author: "Narasima Reddy",
+        Project: "PSA",
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+          <a href="/" className="navbar-brand">
+            React with Component Interaction
+          </a>
+        </nav>
+
+        <React.Fragment>
+          <div className="card m-4">
+            <div className="card-body bg-success text-white">
+              <p className="h5">App Component</p>
+              <span>{JSON.stringify(this.state.userInfo)}</span>
+
+              <UserContext.Provider value={this.state.userInfo}>
+                <CompA />
+              </UserContext.Provider>
+            </div>
+          </div>
+        </React.Fragment>
+      </div>
+    );
+  }
+}
+export default App;
+
 // Style - 1
 /*
 let App = () => {
@@ -10,6 +54,7 @@ function App() {
   return <h1>Hello, Good Evening</h1>;
 }
 */
+/*
 import React from "react";
 
 import Navbar from "./Components/Navbar";
@@ -27,3 +72,4 @@ class App extends React.Component {
   }
 }
 export default App;
+*/
